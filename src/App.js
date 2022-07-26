e77a5394b2c15984498196b236314b5915e88653
Stefan Schlicker <stefanschlicker@posteo.de>
2022-07-26 21:13:19 +0200

    global reset, add custom properties and styled components macro

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+import styled from 'styled-components/macro';
+
 export default function App() {
-  return <div>App goes here</div>;
+  return (
+    <Wrapper>
+      <h1>Willkommen bei Marketing mit Video.</h1>
+      <div>Der Blog ist bald verfügbar.</div>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.main`
+  max-width: 60rem;
+  margin: 0 auto;
+  padding: 3rem;
+  background-color: var(--color-tertiary);
+`;
