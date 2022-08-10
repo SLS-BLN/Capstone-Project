d075ba42848c9bced6a21e24d42b8f591bf05110
Stefan Schlicker <stefanschlicker@posteo.de>
2022-08-10 10:02:37 +0200

    add search-by-tag component

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,7 +1,18 @@
+import SearchByTag from '../components/Search/SearchByTag';
+import styled from 'styled-components/macro';
+
 export default function Search() {
   return (
-    <main style={{padding: '1rem'}}>
-      <h2>Suche</h2>
-    </main>
+    <SearchContainer>
+      <main style={{padding: '1rem'}}>
+        <h2>Suche</h2>
+      </main>
+      <SearchByTag />
+    </SearchContainer>
   );
 }
+
+const SearchContainer = styled.div`
+  margin: 0 3rem;
+  padding-bottom: 2rem;
+`;
