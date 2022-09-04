376e56ee52a2aaed67a4af04ad4820a00b233035
Stefan Schlicker <stefanschlicker@posteo.de>
2022-09-04 12:09:40 +0200

    restore code carousel button

--- src/components/Blogtag/BlogTag.js
+++ src/components/Blogtag/BlogTag.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components/macro';
-import {tagColors} from './BlogTagData';
+import {tagColors} from './BlogTagdata';
 import {useNavigate} from 'react-router-dom';
 
 export function BlogTag({tagName}) {
